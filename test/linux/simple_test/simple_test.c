395354a64d1b119b4f65b703377c944f464d8b6c
Matthias Schoepfer <msc@fp-robotics.com>
2024-07-30 16:32:04 +0200

    minimal working example
    Signed-off-by: Matthias Schoepfer <msc@fp-robotics.com>

--- test/linux/simple_test/simple_test.c
+++ test/linux/simple_test/simple_test.c
@@ -26,6 +26,457 @@ boolean inOP;
 uint8 currentgroup = 0;
 boolean forceByteAlignment = FALSE;
 
+typedef struct __attribute__((packed,aligned(1))) fsoe_safety_master_message_14_bytes_out_T {
+  uint8_t fsoe_master_command; /* FSoE SafetyMasterMessage 14 Bytes Out *Start* */
+  uint8_t fsoe_data1_0 : 1;
+  uint8_t fsoe_data1_1 : 1;
+  uint8_t fsoe_data1_2 : 1;
+  uint8_t fsoe_data1_3 : 1;
+  uint8_t fsoe_data1_4 : 1;
+  uint8_t fsoe_data1_5 : 1;
+  uint8_t fsoe_data1_6 : 1;
+  uint8_t fsoe_data1_7 : 1;
+  uint8_t fsoe_data1_8 : 1;
+  uint8_t fsoe_data1_9 : 1;
+  uint8_t fsoe_data1_10 : 1;
+  uint8_t fsoe_data1_11 : 1;
+  uint8_t fsoe_data1_12 : 1;
+  uint8_t fsoe_data1_13 : 1;
+  uint8_t fsoe_data1_14 : 1;
+  uint8_t fsoe_data1_15 : 1;
+  uint16_t fsoe_master_crc_0;
+  uint8_t fsoe_data1_16 : 1;
+  uint8_t fsoe_data1_17 : 1;
+  uint8_t fsoe_data1_18 : 1;
+  uint8_t fsoe_data1_19 : 1;
+  uint8_t fsoe_data1_20 : 1;
+  uint8_t fsoe_data1_21 : 1;
+  uint8_t fsoe_data1_22 : 1;
+  uint8_t fsoe_data1_23 : 1;
+  uint8_t fsoe_data1_24 : 1;
+  uint8_t fsoe_data1_25 : 1;
+  uint8_t fsoe_data1_26 : 1;
+  uint8_t fsoe_data1_27 : 1;
+  uint8_t fsoe_data1_28 : 1;
+  uint8_t fsoe_data1_29 : 1;
+  uint8_t fsoe_data1_30 : 1;
+  uint8_t fsoe_data1_31 : 1;
+  uint16_t fsoe_master_crc_1;
+  uint8_t fsoe_data1_32 : 1;
+  uint8_t fsoe_data1_33 : 1;
+  uint8_t fsoe_data1_34 : 1;
+  uint8_t fsoe_data1_35 : 1;
+  uint8_t fsoe_data1_36 : 1;
+  uint8_t fsoe_data1_37 : 1;
+  uint8_t fsoe_data1_38 : 1;
+  uint8_t fsoe_data1_39 : 1;
+  uint8_t fsoe_data1_40 : 1;
+  uint8_t fsoe_data1_41 : 1;
+  uint8_t fsoe_data1_42 : 1;
+  uint8_t fsoe_data1_43 : 1;
+  uint8_t fsoe_data1_44 : 1;
+  uint8_t fsoe_data1_45 : 1;
+  uint8_t fsoe_data1_46 : 1;
+  uint8_t fsoe_data1_47 : 1;
+  uint16_t fsoe_master_crc_2;
+  uint8_t fsoe_data1_48 : 1;
+  uint8_t fsoe_data1_49 : 1;
+  uint8_t fsoe_data1_50 : 1;
+  uint8_t fsoe_data1_51 : 1;
+  uint8_t fsoe_data1_52 : 1;
+  uint8_t fsoe_data1_53 : 1;
+  uint8_t fsoe_data1_54 : 1;
+  uint8_t fsoe_data1_55 : 1;
+  uint8_t fsoe_data1_56 : 1;
+  uint8_t fsoe_data1_57 : 1;
+  uint8_t fsoe_data1_58 : 1;
+  uint8_t fsoe_data1_59 : 1;
+  uint8_t fsoe_data1_60 : 1;
+  uint8_t fsoe_data1_61 : 1;
+  uint8_t fsoe_data1_62 : 1;
+  uint8_t fsoe_data1_63 : 1;
+  uint16_t fsoe_master_crc_3;
+  uint8_t fsoe_data1_64 : 1;
+  uint8_t fsoe_data1_65 : 1;
+  uint8_t fsoe_data1_66 : 1;
+  uint8_t fsoe_data1_67 : 1;
+  uint8_t fsoe_data1_68 : 1;
+  uint8_t fsoe_data1_69 : 1;
+  uint8_t fsoe_data1_70 : 1;
+  uint8_t fsoe_data1_71 : 1;
+  uint8_t fsoe_data1_72 : 1;
+  uint8_t fsoe_data1_73 : 1;
+  uint8_t fsoe_data1_74 : 1;
+  uint8_t fsoe_data1_75 : 1;
+  uint8_t fsoe_data1_76 : 1;
+  uint8_t fsoe_data1_77 : 1;
+  uint8_t fsoe_data1_78 : 1;
+  uint8_t fsoe_data1_79 : 1;
+  uint16_t fsoe_master_crc_4;
+  uint8_t fsoe_data1_80 : 1;
+  uint8_t fsoe_data1_81 : 1;
+  uint8_t fsoe_data1_82 : 1;
+  uint8_t fsoe_data1_83 : 1;
+  uint8_t fsoe_data1_84 : 1;
+  uint8_t fsoe_data1_85 : 1;
+  uint8_t fsoe_data1_86 : 1;
+  uint8_t fsoe_data1_87 : 1;
+  uint8_t fsoe_data1_88 : 1;
+  uint8_t fsoe_data1_89 : 1;
+  uint8_t fsoe_data1_90 : 1;
+  uint8_t fsoe_data1_91 : 1;
+  uint8_t fsoe_data1_92 : 1;
+  uint8_t fsoe_data1_93 : 1;
+  uint8_t fsoe_data1_94 : 1;
+  uint8_t fsoe_data1_95 : 1;
+  uint16_t fsoe_master_crc_5;
+  uint8_t fsoe_data1_96 : 1;
+  uint8_t fsoe_data1_97 : 1;
+  uint8_t fsoe_data1_98 : 1;
+  uint8_t fsoe_data1_99 : 1;
+  uint8_t fsoe_data1_100 : 1;
+  uint8_t fsoe_data1_101 : 1;
+  uint8_t fsoe_data1_102 : 1;
+  uint8_t fsoe_data1_103 : 1;
+  uint8_t fsoe_data1_104 : 1;
+  uint8_t fsoe_data1_105 : 1;
+  uint8_t fsoe_data1_106 : 1;
+  uint8_t fsoe_data1_107 : 1;
+  uint8_t fsoe_data1_108 : 1;
+  uint8_t fsoe_data1_109 : 1;
+  uint8_t fsoe_data1_110 : 1;
+  uint8_t fsoe_data1_111 : 1;
+  uint16_t fsoe_master_crc_6;
+  uint16_t fsoe_master_connection_id;  /* FSoE SafetyMasterMessage 14 Bytes Out *End* */
+} fsoe_safety_master_message_14_bytes_out_t;
+
+
+typedef struct __attribute__((packed,aligned(1)))  master_to_master_communication_inputs_T {
+  uint64_t smmc_inputs1;
+  uint64_t smmc_inputs2;
+  uint64_t smmc_inputs3;
+  uint64_t smmc_inputs4;
+} master_to_master_communication_inputs_t;
+
+typedef struct __attribute__((packed,aligned(1))) functional_inputs_T {
+  uint8_t byte_in_0;  /* Functional Inputs Start */
+  uint8_t byte_in_1;
+  uint8_t byte_in_2;
+  uint8_t byte_in_3;
+  uint8_t byte_in_4;
+  uint8_t byte_in_5;
+  uint8_t byte_in_6;
+  uint8_t byte_in_7;
+  uint8_t byte_in_8;
+  uint8_t byte_in_9;
+  uint8_t byte_in_10;
+  uint8_t byte_in_11;
+  uint8_t byte_in_12;
+  uint8_t byte_in_13;
+  uint8_t byte_in_14;
+  uint8_t byte_in_15;
+  uint8_t byte_in_16;
+  uint8_t byte_in_17;  /* Functional Inputs End */
+} functional_inputs_t;
+
+typedef struct __attribute__((packed,aligned(1))) fsoe_safety_slave_message_4_bytes_in_T {
+  uint8_t fsoe_slave_command; /* FSoE SafetySlaveMessage 04 Bytes In *Start* */
+  uint8_t fsoe_data1_0 : 1;
+  uint8_t fsoe_data1_1 : 1;
+  uint8_t fsoe_data1_2 : 1;
+  uint8_t fsoe_data1_3 : 1;
+  uint8_t fsoe_data1_4 : 1;
+  uint8_t fsoe_data1_5 : 1;
+  uint8_t fsoe_data1_6 : 1;
+  uint8_t fsoe_data1_7 : 1;
+  uint8_t fsoe_data1_8 : 1;
+  uint8_t fsoe_data1_9 : 1;
+  uint8_t fsoe_data1_10 : 1;
+  uint8_t fsoe_data1_11 : 1;
+  uint8_t fsoe_data1_12 : 1;
+  uint8_t fsoe_data1_13 : 1;
+  uint8_t fsoe_data1_14 : 1;
+  uint8_t fsoe_data1_15 : 1;
+  uint16_t fsoe_slave_crc_0;
+  uint8_t fsoe_data1_16 : 1;
+  uint8_t fsoe_data1_17 : 1;
+  uint8_t fsoe_data1_18 : 1;
+  uint8_t fsoe_data1_19 : 1;
+  uint8_t fsoe_data1_20 : 1;
+  uint8_t fsoe_data1_21 : 1;
+  uint8_t fsoe_data1_22 : 1;
+  uint8_t fsoe_data1_23 : 1;
+  uint8_t fsoe_data1_24 : 1;
+  uint8_t fsoe_data1_25 : 1;
+  uint8_t fsoe_data1_26 : 1;
+  uint8_t fsoe_data1_27 : 1;
+  uint8_t fsoe_data1_28 : 1;
+  uint8_t fsoe_data1_29 : 1;
+  uint8_t fsoe_data1_30 : 1;
+  uint8_t fsoe_data1_31 : 1;
+  uint16_t fsoe_slave_crc_1;
+  uint16_t fsoe_connection_id; /* FSoE SafetySlaveMessage 04 Bytes In *End* */
+} fsoe_safety_slave_message_4_bytes_in_t;
+
+typedef struct __attribute__((packed,aligned(1))) master_to_master_communication_outputs_T {
+  uint64_t smmc_outputs;
+} master_to_master_communication_outputs_t;
+
+typedef struct __attribute__((packed,aligned(1))) functional_outputs_T {
+  uint16_t status_word; /* Functional Outputs Start */
+  uint8_t debug_0;
+  uint8_t debug_1;
+  uint8_t debug_2;
+  uint8_t debug_3;
+  uint8_t debug_4;
+  uint8_t byte_out_0;
+  uint8_t byte_out_1;
+  uint8_t byte_out_2;
+  uint8_t byte_out_3;
+  uint8_t byte_out_4;
+  uint8_t byte_out_5;
+  uint8_t byte_out_6;
+  uint8_t byte_out_7;
+  uint8_t byte_out_8;
+  uint8_t byte_out_9;
+  uint8_t byte_out_10;
+  uint8_t byte_out_11;
+  uint8_t byte_out_12;
+  uint8_t byte_out_13;
+  uint8_t byte_out_14;
+  uint8_t byte_out_15;
+  uint8_t byte_out_16;
+  uint8_t byte_out_17;
+  uint8_t byte_out_18;
+  uint8_t byte_out_19;
+  uint8_t byte_out_20;
+  uint8_t byte_out_21;
+  uint8_t byte_out_22;
+  uint8_t byte_out_23;
+  uint8_t byte_out_24;
+  uint8_t byte_out_25;
+  uint8_t byte_out_26;
+} functional_outputs_t;
+
+
+typedef struct __attribute__((packed,aligned(1))) slave_0_pdo_mapping_t {
+  fsoe_safety_slave_message_4_bytes_in_t fsoe_safety_slave_message_4_bytes_in;
+  uint8_t alignment_byte_conn1;
+  master_to_master_communication_outputs_t master_to_master_communication_outputs;
+  functional_outputs_t functional_outputs;
+  fsoe_safety_master_message_14_bytes_out_t fsoe_safety_master_message_14_bytes_out;
+  uint8_t alignment_byte_conn1_1;
+  master_to_master_communication_inputs_t master_to_master_communications_inputs;
+  functional_inputs_t functional_inputs;
+} bbh_slave_mapping;
+
+typedef struct __attribute__((packed,aligned(1))) bbh_write_mapping_T {
+  fsoe_safety_master_message_14_bytes_out_t fsoe_safety_master_message_14_bytes_out;
+  uint8_t alignment_byte_conn1_1;
+  master_to_master_communication_inputs_t master_to_master_communications_inputs;
+  functional_inputs_t functional_inputs;
+} bbh_write_mapping_t;
+
+typedef struct __attribute__((packed,aligned(1))) bbh_read_mapping_T {
+  fsoe_safety_slave_message_4_bytes_in_t fsoe_safety_slave_message_4_bytes_in;
+  uint8_t alignment_byte_conn1;
+  master_to_master_communication_outputs_t master_to_master_communication_outputs;
+  functional_outputs_t functional_outputs;
+} bbh_read_mapping_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia402_rx_T {
+  uint16_t control_word;
+  uint8_t modes_of_operation;
+  int16_t target_torque;
+  uint32_t target_position;
+  int32_t target_velocity;
+  uint16_t torque_offset;
+  uint32_t tuning_command; /* RxPDO Mapping 1 End */
+} cia_402_rx_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia_402_rx2_T {
+  uint32_t pysical_output;
+  uint32_t bit_mask;
+} cia_402_rx2_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia_402_rx3_T {
+  uint32_t user_mosi;
+  uint32_t velocity_offsets;
+} cia_402_rx3_t;
+
+typedef struct __attribute__((packed,aligned(1))) plc_to_drive_T {
+  uint8_t fsoe_command;
+  uint8_t fsoe_sto :1;
+  uint8_t fsoe_ss1 :1;
+  uint8_t fsoe_ss2 :1;
+  uint8_t fsoe_sos :1;
+  uint8_t fsoe_pad1 :3;
+  uint8_t fsoe_error_acknowledge :1;
+  uint8_t fsoe_sls_instance_1 :1;
+  uint8_t fsoe_sls_instance_2 :1;
+  uint8_t fsoe_sls_instance_3 :1;
+  uint8_t fsoe_sls_instance_4 :1;
+  uint8_t fsoe_restart_acknowledge :1;
+  uint8_t fsoe_sbc_command :1;
+  uint8_t fsoe_reset_position :1;
+  uint8_t fsoe_pad2 :1;
+  uint16_t fsoe_crc_0;
+  uint8_t fsoe_pad3;
+  uint8_t fsoe_pad4 :4;
+  uint8_t fsoe_safe_output_1 :1;
+  uint8_t fsoe_safe_output_2 :1;
+  uint8_t fsoe_pad5 : 2;
+  uint16_t fsoe_crc_1;
+  uint16_t fsoe_connection_id;
+} plc_to_drive_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia_402_tx_T {
+  uint16_t status_word;
+  uint8_t modes_of_operation_display;
+  uint32_t position_actual_value;
+  int32_t velocity_actual_value;
+  int16_t torque_actual_value;
+} cia_402_tx_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia_402_tx2_T {
+  int16_t analog_input_1;
+  int16_t analog_input_2;
+  int16_t analog_input_3;
+  int16_t analog_input_4;
+  uint32_t tuning_status;
+} cia_402_tx2_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia_402_tx3_T {
+  uint32_t digital_inputs;
+} cia_402_tx3_t;
+
+typedef struct __attribute__((packed,aligned(1))) cia_402_tx4_T {
+  uint32_t user_miso;
+  uint32_t timestamp;
+  uint32_t position_demand_internal_value;
+  uint32_t velocity_demand_value;
+  uint16_t torque_demand;
+} cia_402_tx4_t;
+
+typedef struct __attribute__((packed,aligned(1))) drive_to_plc_T {
+  uint8_t fsoe_command;
+  uint8_t fsoe_sto_state :1;
+  uint8_t fsoe_pad1 :2;
+  uint8_t fsoe_sos_state :1;
+  uint8_t fsoe_pad2 :3;
+  uint8_t fsoe_error_state :1;
+  uint8_t fsoe_ss1_state :1;
+  uint8_t fsoe_ss2_state :1;
+  uint8_t fsoe_pad3 :2;
+  uint8_t fsoe_sls_instance_1 :1;
+  uint8_t fsoe_sls_instance_2 :1;
+  uint8_t fsoe_sls_instance_3 :1;
+  uint8_t fsoe_sls_instance_4 :1;
+  uint16_t fsoe_crc_0;
+  uint8_t fsoe_restart_acknowledge :1;
+  uint8_t fsoe_sbs_state :1;
+  uint8_t fsoe_temperature_warning :1;
+  uint8_t fsoe_safe_position_valid :1;
+  uint8_t fsoe_safe_speed_valid :1;
+  uint8_t fsoe_pad4 :3;
+  uint8_t fsoe_safe_input_1 :1;
+  uint8_t fsoe_safe_input_2 :1;
+  uint8_t fsoe_safe_input_3 :1;
+  uint8_t fsoe_safe_input_4 :1;
+  uint8_t fsoe_safe_output_monitor_1 :1;
+  uint8_t fsoe_safe_output_monitor_2 :1;
+  uint8_t fsoe_analog_diagnostic_active :1;
+  uint8_t fsoe_analog_value_valid :1;
+  uint16_t fsoe_crc_1;
+  uint16_t fsoe_safe_position_actual_value;
+  uint16_t fsoe_crc_2;
+  uint16_t fsoe_pad5;
+  uint16_t fsoe_crc_3;
+  int16_t fsoe_save_velocity_actual_value;
+  uint16_t fsoe_crc_4;
+  uint16_t fsoe_pad6;
+  uint16_t fsoe_crc_5;
+  int16_t fsoe_safe_analog_value_scaled;
+  uint16_t fsoe_crc_6;
+  uint16_t fsoe_connection_id;
+} drive_to_plc_t;
+
+
+
+typedef struct __attribute__((packed,aligned(1))) slave_1_pdo_mapping_T {
+  cia_402_rx_t cia_402_rx;
+  cia_402_rx2_t cia_402_rx2;
+  cia_402_rx3_t cia_402_rx3;
+  plc_to_drive_t plc_to_drive;
+  uint8_t padding_modulePdoAling1Out;
+  /* RxPDO Mapping stop */
+  cia_402_tx_t cia_402_tx;
+  cia_402_tx2_t cia_402_tx2;
+  cia_402_tx3_t cia_402_tx3;
+  cia_402_tx4_t cia_402_tx4;
+  drive_to_plc_t drive_to_plc;
+} slave_1_pdo_mapping_t;
+
+typedef struct __attribute__((packed,aligned(1))) synapticon_read_mapping_T {
+  cia_402_tx_t cia_402_tx;
+  cia_402_tx2_t cia_402_tx2;
+  cia_402_tx3_t cia_402_tx3;
+  cia_402_tx4_t cia_402_tx4;
+  drive_to_plc_t drive_to_plc;
+} synapticon_read_mapping_t;
+
+typedef struct __attribute__((packed,aligned(1))) synapticon_write_mapping_T {
+  cia_402_rx_t cia_402_rx;
+  cia_402_rx2_t cia_402_rx2;
+  cia_402_rx3_t cia_402_rx3;
+  plc_to_drive_t plc_to_drive;
+} synapticon_write_mapping_t;
+
+
+
+static const uint32_t initModulesBBH[] = {0x6212, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                                          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                                          0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                                          0x0, 0x0, 0x0, 0x6194, 0x6195};
+
+static const uint32_t initModulesSynapticon[] = {0x04020001, 0x22D20001};
+
+int BBHsetup(uint16 slave) {
+    printf("Executing BBHSetup\n");
+    uint8_t initModulesBBHSize = (uint8_t) sizeof(initModulesBBH) / sizeof(initModulesBBH[0]);
+    ec_slave[slave].SM[2].SMflags = htoel(0x64);
+    ec_slave[slave].SM[2].SMlength = htoes(82);
+    ec_slave[slave].SM[2].StartAddr = htoes(0x1200);
+    ec_slave[slave].SM[3].SMflags = htoel(0x20);
+    ec_slave[slave].SM[3].SMlength = htoes(54);
+    ec_slave[slave].SM[3].StartAddr = htoes(0x1800);
+
+    ec_slave[slave].SMtype[2] = 3;
+    ec_slave[slave].SMtype[3] = 4;
+
+    for (size_t i = 0; i < initModulesBBHSize; ++i) {
+      ec_SDOwrite(slave, 0xf030, (uint8_t) i + 1, FALSE, sizeof(uint32_t), &initModulesBBH[i],
+                             EC_TIMEOUTSAFE);
+    }
+    ec_SDOwrite(slave, 0xf030, 0, FALSE, sizeof(uint8_t), &initModulesBBHSize, EC_TIMEOUTSAFE);
+
+    ec_slave[slave].configindex = 1;
+    ec_slave[slave].Ibits = ec_slave[slave].SM[3].SMlength*8;
+    ec_slave[slave].Obits = ec_slave[slave].SM[2].SMlength*8;
+    return 1;
+}
+
+int SynapticonSetup(uint16_t slave) {
+    uint8_t initModulesSynapticonSize = (uint8_t) sizeof(initModulesSynapticon) / sizeof(initModulesSynapticon[0]);
+    for (size_t i = 0; i < initModulesSynapticonSize; ++i) {
+        ec_SDOwrite(slave, 0xf030, (uint8_t) i + 1, FALSE, sizeof(uint32_t), &initModulesSynapticon[i],
+                             EC_TIMEOUTSAFE);
+    }
+    ec_SDOwrite(slave, 0xf030, 0, FALSE, sizeof(uint8_t), &initModulesSynapticonSize, EC_TIMEOUTSAFE);
+    return 1;
+}
+
+
 void simpletest(char *ifname)
 {
     int i, j, oloop, iloop, chk;
@@ -38,12 +489,18 @@ void simpletest(char *ifname)
    if (ec_init(ifname))
    {
       printf("ec_init on %s succeeded.\n",ifname);
+
       /* find and auto-config slaves */
 
 
        if ( ec_config_init(FALSE) > 0 )
       {
+         /* Set special functions for BBH and Synapticon */
+         ec_slave[1].PO2SOconfig = BBHsetup;
+         ec_slave[2].PO2SOconfig = SynapticonSetup;
+
          printf("%d slaves found and configured.\n",ec_slavecount);
+
 
          if (forceByteAlignment)
          {
@@ -94,6 +551,8 @@ void simpletest(char *ifname)
                 /* cyclic loop */
             for(i = 1; i <= 10000; i++)
             {
+               memcpy(&((bbh_write_mapping_t*) ec_slave[1].outputs)->fsoe_safety_master_message_14_bytes_out,&(((synapticon_read_mapping_t*) ec_slave[2].inputs)->drive_to_plc),sizeof(drive_to_plc_t));
+               memcpy(&((synapticon_write_mapping_t*) ec_slave[2].outputs)->plc_to_drive,&((bbh_read_mapping_t*) ec_slave[1].inputs)->fsoe_safety_slave_message_4_bytes_in,sizeof(plc_to_drive_t));
                ec_send_processdata();
                wkc = ec_receive_processdata(EC_TIMEOUTRET);
 
@@ -114,7 +573,7 @@ void simpletest(char *ifname)
                         printf(" T:%"PRId64"\r",ec_DCtime);
                         needlf = TRUE;
                     }
-                    osal_usleep(5000);
+                    osal_usleep(2000);
 
                 }
                 inOP = FALSE;
